1058f62f8b90e085cfc062d7ecad65bf399371ac
Michelle Tolentino <misheruvynx@gmail.com>
2025-03-04 18:07:18 +0800

    added shell structure

--- src/Shell-app.jsx
+++ src/Shell-app.jsx
@@ -5,20 +5,34 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className='shell-container'>
+      {/* collapsible menu */}
+
+      {/* static left navi -- icons */}
+      <div className='sidebar-icons-container'>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {/* collapsible description navi */}
+      <div className='sidebar-desc-container'>
+
+      </div>
+
+
+      
+      {/* adjustable right content */}
+      <div className='header-body-container'>
+        <div className='header-navi'>
+
+        </div>
+        <div className='body-container'>
+
+        </div>
+      </div>
+
+      
+
+    </div>
   )
 }
 
