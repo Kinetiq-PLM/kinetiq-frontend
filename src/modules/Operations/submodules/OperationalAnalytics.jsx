4c940afdbbf45362b32c3d7c6ddff717f019895a
Michelle Tolentino <misheruvynx@gmail.com>
2025-03-07 17:13:33 +0800

    fixed css namespace assignments

--- src/modules/Operations/submodules/OperationalAnalytics.jsx
+++ src/modules/Operations/submodules/OperationalAnalytics.jsx
@@ -3,11 +3,11 @@ import "../styles/OperationalAnalytics.css";
 
 const BodyContent = () => {
     return (
-        <div className="opanal">
+        <div className="opanalysis">
             <div className="body-content-container">
                 <p>Hello Operational Analysis SubModule!</p>
                 <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.opanal' at the start.</p>
+                <p>If you're going to style with css, use your unique namespace '.opanalysis' at the start.</p>
             </div>
         </div>
 
