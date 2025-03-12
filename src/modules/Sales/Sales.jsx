ed0ba2e15db36706635ca59ba70de80a73b5ab0e
CoderTofu <vioneverforget@gmail.com>
2025-03-12 18:36:49 +0800

    added sales dashboard

--- src/modules/Sales/Sales.jsx
+++ src/modules/Sales/Sales.jsx
@@ -1,28 +1,41 @@
 import React from "react";
-import { lazy } from "react";
 import "./styles/Index.css";
+import Heading from "./components/Heading";
 
 const BodyContent = ({ loadSubModule, setActiveSubModule }) => {
+  const salesSubModule = {
+    Quotation: "Quotation",
+    Order: "Order",
+    Delivery: "Delivery",
+    Invoice: "Invoice",
+    "Master List": "MasterList",
+    Dunning: "Dunning",
+    Reporting: "Reporting",
+    Returns: "Returns",
+  };
+
   return (
     <div className="sales">
       <div className="body-content-container">
-        <p>Hello Sales Module!</p>
-        <p>
-          Fill this container with your elements, change the display if need be.
-        </p>
-        <p>
-          If you're going to style with css, use your unique namespace '.sales'
-          at the start.
-        </p>
-        <button
-          onClick={() => {
-            setActiveSubModule("Invoices");
-            loadSubModule("Invoices");
-          }}
-          className="sales-nav-button cursor-pointer bg-blue-400 mt-5"
-        >
-          Go to Invoices
-        </button>
+        <Heading
+          Title="Sales Dashboard"
+          SubTitle="Your shortcut to all sales operations."
+        />
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+          {Object.entries(salesSubModule).map(([key, value]) => (
+            <button
+              key={value}
+              className="flex items-center justify-center bg-[#FAFAFA] rounded-lg border border-gray-200 p-10 shadow-sm hover:shadow-md transition-shadow duration-200 cursor-pointer font-bold text-xl"
+              onClick={() => {
+                loadSubModule(key);
+                setActiveSubModule(key);
+              }}
+            >
+              <span className="font-medium text-gray-800">{key}</span>
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
