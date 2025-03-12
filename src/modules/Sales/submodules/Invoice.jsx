e9eac0defec5f28b55a355ed9f7b3d0671bdc8f1
CoderTofu <vioneverforget@gmail.com>
2025-03-12 17:32:48 +0800

    boilerplate for sales module fe

--- src/modules/Sales/submodules/Invoice.jsx
+++ src/modules/Sales/submodules/Invoice.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { lazy } from "react";
-import "./styles/Index.css";
+import "../styles/Index.css";
 
 const BodyContent = ({ loadSubModule, setActiveSubModule }) => {
   return (
-    <div className="sales">
+    <div className="invoice">
       <div className="body-content-container">
-        <p>Hello Sales Module!</p>
+        <p>Hello Invoices SubModule!</p>
         <p>
           Fill this container with your elements, change the display if need be.
         </p>
         <p>
-          If you're going to style with css, use your unique namespace '.sales'
-          at the start.
+          If you're going to style with css, use your unique namespace
+          '.invoices' at the start.
         </p>
         <button
           onClick={() => {
-            setActiveSubModule("Invoices");
-            loadSubModule("Invoices");
+            setActiveSubModule("Quotation");
+            loadSubModule("Quotation");
           }}
           className="sales-nav-button cursor-pointer bg-blue-400 mt-5"
         >
-          Go to Invoices
+          Go to Quotations
         </button>
       </div>
     </div>
