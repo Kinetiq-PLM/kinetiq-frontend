4818070b3bdd071d3fdab80119feae4fc6069cae
Samantha Aguinaldo <ssaguinaldo2022@plm.edu.ph>
2025-03-16 16:51:22 +0800

    modified submodules

--- src/modules/Operations/submodules/GR.jsx
+++ src/modules/Operations/submodules/GR.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import "../styles/WorkflowAutomation.css";
+import "../styles/GR.css";
 
 const BodyContent = () => {
     return (
-        <div className="workauto">
+        <div className="gr">
             <div className="body-content-container">
-                <p>Hello Workflow Automation SubModule!</p>
+                <p>Hello Goods Receipt SubModule!</p>
                 <p>Fill this container with your elements, change the display if need be.</p>
                 <p>If you're going to style with css, use your unique namespace '.workauto' at the start.</p>
             </div>
