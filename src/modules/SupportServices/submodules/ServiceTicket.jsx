69a7b662559c1aeb14254be7d3661f09a98cdfd6
Paula Manalo <paulamaemanalo@gmail.com>
2025-03-22 13:38:51 +0800

    Added boilerplate for Support & Services submodules

---
+++ src/modules/SupportServices/submodules/ServiceTicket.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/ServiceTicket.css";
+
+const BodyContent = () => {
+    return (
+        <div className="servtick">
+            <div className="body-content-container">
+                <p>Hello Support & Services Module!</p>
+                <p>Fill this container with your elements, change the display if need be.</p>
+                <p>If you're going to style with css, use your unique namespace '.serv' at the start.</p>
+            </div>
+        </div>
+    );
+};
+
+export default BodyContent;
