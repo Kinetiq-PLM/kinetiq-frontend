d8f8c7a1575807579d9c402067377417d7e1275b
Tristan <tgjvocampo2021@plm.edu.ph>
2025-03-24 14:47:30 +0800

    New Submodules
    - Removed "Product Catalog" Sub Module
    - Added "P-Counts" and "Stock Flow" Sub Modules

---
+++ src/modules/Inventory/submodules/StockFlow.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/StockFlow.css";
+
+const BodyContent = () => {
+    return (
+        <div className="stockflow">
+            <div className="body-content-container">
+                <p>Hello Product Catalog SubModule!</p>
+                <p>Fill this container with your elements, change the display if need be.</p>
+                <p>If you're going to style with css, use your unique namespace '.stockflow' at the start.</p>
+            </div>
+        </div>
+
+    );
+};
+
+export default BodyContent;
