a95fcafe1723d21c834e843d02e35e23998878ca
IEMDomain04 <emmanman0011@gmail.com>
2025-03-31 16:57:11 +0800

    Added Accounts payable and receivable

--- src/modules/Accounting/submodules/AccountsPayable.jsx
+++ src/modules/Accounting/submodules/AccountsPayable.jsx
@@ -13,7 +13,7 @@ const AccountsPayable = () => {
 
                 <div className="title-subtitle-container">
                     <h1 className="subModule-title">Accounts Payable</h1>
-                    <h2 className="subModule-subTitle">List of accounts payable in this system.</h2>
+                    <h2 className="subModule-subTitle">Detailed list of accounts payable.</h2>
                 </div>
 
                 <div className="parent-component-container">
--- src/modules/Accounting/submodules/AccountsReceivable.jsx
+++ src/modules/Accounting/submodules/AccountsReceivable.jsx
@@ -13,8 +13,8 @@ const AccountsReceivable = () => {
       <div className="body-content-container">
 
         <div className="title-subtitle-container">
-          <h1 className="subModule-title">Official Receipts</h1>
-          <h2 className="subModule-subTitle">List of receipts.</h2>
+          <h1 className="subModule-title">Accounts Receivable</h1>
+          <h2 className="subModule-subTitle">Detailed list of accounts receivable.</h2>
         </div>
 
         <div className="parent-component-container">
