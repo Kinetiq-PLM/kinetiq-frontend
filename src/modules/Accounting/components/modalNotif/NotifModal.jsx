b4cb9b73b2b37fd661c3537986fe5a9d893cf53e
IEMDomain04 <emmanman0011@gmail.com>
2025-03-31 18:02:13 +0800

    Adjusted Modal

--- src/modules/Accounting/components/modalNotif/NotifModal.jsx
+++ src/modules/Accounting/components/modalNotif/NotifModal.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Button from '../Button';
 
 const NotifModal = ({ isOpen, onClose, type = "success", title, message }) => {
     const [showModal, setShowModal] = useState(isOpen);
@@ -9,7 +10,7 @@ const NotifModal = ({ isOpen, onClose, type = "success", title, message }) => {
             const timer = setTimeout(() => {
                 setShowModal(false);
                 if (onClose) onClose();
-            }, 1000);
+            }, 30000); // 30sec
             return () => clearTimeout(timer);
         }
     }, [isOpen, onClose]);
@@ -26,10 +27,10 @@ const NotifModal = ({ isOpen, onClose, type = "success", title, message }) => {
         <div className="fixed inset-0 bg-black/30 flex items-center justify-center z-[9999]">
 
 
-            <div className="bg-white p-20 rounded-lg shadow-lg relative z-[10000] transition-opacity duration-300">
+            <div className="bg-white p-10 rounded-lg shadow-lg relative z-[10000] transition-opacity duration-300">
 
 
-                <div className="flex flex-col items-center">
+                <div className="max-w-60 flex flex-col items-center">
 
                     
                     <img className='my-5' width={80} height={80} src={iconSrc} alt={type} />
@@ -38,6 +39,7 @@ const NotifModal = ({ isOpen, onClose, type = "success", title, message }) => {
                     <div className='space-y-5 text-center'>
                         <h1 className='text-2xl font-bold'>{title}</h1>
                         <p className='text-gray-500'>{message}</p>
+                        <Button name="Close" variant="standard2" onclick={onClose}/>
                     </div>
                 </div>
             </div>
