7e9e61a0f7e019a39f3ed89de0bc887259d3f85e
CoderTofu <vioneverforget@gmail.com>
2025-04-03 18:41:33 +0800

    minor ui tweak

--- src/modules/Sales/components/Button.jsx
+++ src/modules/Sales/components/Button.jsx
@@ -9,7 +9,7 @@ const Button = ({
   submit = false,
 }) => {
   let styling =
-    "h-[38px] py-1 px-6 font-medium transition-all duration-300 ease-in-out transform";
+    "py-1 px-6 font-medium transition-all duration-300 ease-in-out transform";
 
   if (type === "primary") {
     styling +=
