58c401cc02e40e3a8033cec6bc14af5e4b6fed86
marckienriquez <112945854+marckienriquez@users.noreply.github.com>
2025-04-12 01:26:37 +0800

    Revert "Accounting/main"

--- src/App.jsx
+++ src/App.jsx
@@ -97,14 +97,9 @@ function App() {
       "User Accounts": "UserAccounts",
     },
     Accounting: {
-      "Chart of Accounts": "ChartOfAccounts",
-      "Journal": "Journal",
-      "Journal Entry": "JournalEntry",
       "General Ledger": "GeneralLedger",
-      "General Ledger Accounts": "GeneralLedgerAccounts",
-      "Accounts Receivable": "AccountsReceivable",
       "Accounts Payable": "AccountsPayable",
-      "Official Receipts": "OfficialReceipts",
+      "Accounts Receivable": "AccountsReceivable",
     },
     Financials: {
       Budgeting: "Budgeting",
