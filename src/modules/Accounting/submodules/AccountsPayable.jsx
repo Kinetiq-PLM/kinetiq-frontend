6add9d9840d7b8ade39c6ec16532941f084e1335
alex <122758355+alexong04@users.noreply.github.com>
2025-04-13 01:30:56 +0800

    merge accounting module

--- src/modules/Accounting/submodules/AccountsPayable.jsx
+++ src/modules/Accounting/submodules/AccountsPayable.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import "../styles/AccountsPayable.css";
+import React from 'react'
+import '../styles/accounting-styling.css'
+import Table from '../components/Table'
+import Search from '../components/Search'
 
-const BodyContent = () => {
+const AccountsPayable = () => {
+    const columns = ["JERICHO Lagay ka columns here."]
+
+    const data = ["JERICHO Lagay ka data here."]
     return (
-        <div className="acctpay">
+        <div className="accountsPayable">
             <div className="body-content-container">
-                <p>Hello Accounts Payable SubModule!</p>
-                <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.acctpay' at the start.</p>
+
+                <div className="title-subtitle-container">
+                    <h1 className="subModule-title">Accounts Payable</h1>
+                </div>
+
+                <div className="parent-component-container">
+                    <Search type="text" placeholder="Search Record.." />
+                </div>
+
+                {/* <Table data={data} columns={columns} enableCheckbox={false} /> */}
+
             </div>
-
         </div>
+    )
+}
 
-
-    );
-};
-
-export default BodyContent;
+export default AccountsPayable
