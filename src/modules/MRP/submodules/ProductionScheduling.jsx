c68e6b6903fd2713cc6ee2f8671ea067fc5ab397
gekiiMei <gekimeshi@gmail.com>
2025-04-12 22:41:07 +0800

    Revert "Merge branch 'IT/initial-merge' into HumanResources/main"
    This reverts commit 7c40f27b1c7be9e63bc50e47777152e5abf17e7b, reversing
    changes made to cc684a670d7178d2d10029e79997a7519625a7e5.

--- src/modules/MRP/submodules/ProductionScheduling.jsx
+++ src/modules/MRP/submodules/ProductionScheduling.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import "./styles/Production.css";
+import "../styles/ProductionScheduling.css";
 
 const BodyContent = () => {
     return (
-        <div className="prod">
+        <div className="prodsched">
             <div className="body-content-container">
-                <p>Hello Purchasing Module!</p>
+                <p>Hello Production Scheduling SubModule!</p>
                 <p>Fill this container with your elements, change the display if need be.</p>
-                <p>If you're going to style with css, use your unique namespace '.purch' at the start.</p>
+                <p>If you're going to style with css, use your unique namespace '.prodsched' at the start.</p>
             </div>
         </div>
+
     );
 };
 
