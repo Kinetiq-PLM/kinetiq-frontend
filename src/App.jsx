de8bb1d982ce1af33781674ba15637333ece7c9a
Joan675 <joangamba39@gmail.com>
2025-04-12 07:29:52 -0700

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -80,14 +80,7 @@ function App() {
   const moduleSubmoduleFileNames = {
     Management: {
       "Dashboard": "ManagementDashboard",
-      "Policy Compliance Oversight": "ManagementPolicyComplianceOversight",
-      "Salary Release Approval": "ManagementSalaryReleaseApproval",
-      "Budget Review Approval": "ManagementBudgetReviewApproval",
-      "Purchasing Approval": "ManagementPurchasingApproval",
-      "Project Approval": "ManagementProjectApproval",
-      "Project Monitoring": "ManagementProjectMonitoring",
-      "RecruitmentCandidates": "ManagementRecruitmentCandidates",
-      "AssetRemoval": "ManagementAssetRemoval",
+      "Project Approval": "ManagementApprovals",
       "User Roles": "UserRoles",
       "Access Control": "AccessControl",
       Settings: "Settings",
