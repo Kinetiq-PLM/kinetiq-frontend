a86a0542dd8d786884588307156550c64c3cfbc5
gekiiMei <gekimeshi@gmail.com>
2025-04-14 18:17:29 +0800

    notif update

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import SearchBar from "./shared/components/SearchBar";
 import UserProfile from "./shared/components/UserProfile";
 import { Navigate, useNavigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { User } from "lucide-react";
 
 function App() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [hasNotification, setHasNotification] = useState(true);
+  const [hasNotification, setHasNotification] = useState(false);
   const [activeModule, setActiveModule] = useState(null);
   const [activeSubModule, setActiveSubModule] = useState(null);
   const [hoveredModule, setHoveredModule] = useState(null);
@@ -19,6 +20,7 @@ function App() {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [showUserProfile, setShowUserProfile] = useState(false);
+  const [notifs, setNotifs] = useState([]);
 
   const displayName = user
     ? `${user.first_name} ${user.last_name?.charAt(0)}.`
@@ -71,74 +73,135 @@ function App() {
     return () => document.removeEventListener("click", handleClickOutside);
   }, []);
 
+  //fetch notifs
+  const fetchNotifs = async (user) => {
+    console.log("Fetching notifs...")
+    const resp = await fetch(`http://127.0.0.1:8000/api/notifications/?user_id=${user?.user_id}`, {method: 'GET'})
+    // const resp_text = await resp.text()
+    // console.log("resp text")
+    // console.log(resp_text)
+    const resp_data = await resp.json();
+    const notif_items = resp_data.data
+    console.log("Notifs fetched:")
+    console.log(notif_items)
+    var temp_list = []
+    //populate notifs table
+    notif_items.map((notif_item, i) => {
+      origin = notif_item.module.split('/')
+      const orig_module = origin[0]
+      const orig_submodule = origin.length == 2 ? origin[1] : null  
+      const time_formatted = new Date(notif_item.created_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
+      temp_list[i] = {
+        id: notif_item.notifications_id,
+        msg: notif_item.message,
+        orig_module: orig_module,
+        orig_submodule: orig_submodule,
+        read: notif_item.notifications_status == 'Read',
+        time: time_formatted
+      }
+    })
+    setNotifs(temp_list)
+    console.log('Final notif list:')
+    console.log(temp_list)
+
+    //notif icon toggle (for loop so we can break out)
+    for (var i=0; i<temp_list.length; ++i) {
+      if (temp_list[i].read == false) {
+        console.log('found notif')
+        setHasNotification(true)
+        break
+      }
+    }
+  }
+
+  //func for marking notifs as read
+  const readNotif = async (notif_id) => {
+    const resp = await fetch(`http://127.0.0.1:8000/api/notifications/`, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        notifications_id: notif_id
+      })
+    })
+  }
+
+  //get notifs
+  useEffect(() => {
+    if (user) {
+      fetchNotifs(user)
+    }
+  }, [user]);
+
 
   //dummy notifs
-  const notifs = [
-    {
-      time: "8:00 PM",
-      msg: "Ur phone ringing!!!",
-      orig_module: "Administration",
-      orig_submodule: null,
-      read: false
-    },
-    {
-      time: "9:00 PM",
-      msg: "keep urself safe!!!",
-      orig_module: "Sales",
-      orig_submodule: null,
-      read: false
-    },
-    {
-      time: "8:00 PM",
-      msg: "bibidi bobidi boo wah",
-      orig_module: "Management",
-      orig_submodule: "Access Control",
-      read: false
+  // const notifs = [
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "Ur phone ringing!!!",
+  //     orig_module: "Administration",
+  //     orig_submodule: null,
+  //     read: false
+  //   },
+  //   {
+  //     time: "9:00 PM",
+  //     msg: "keep urself safe!!!",
+  //     orig_module: "Sales",
+  //     orig_submodule: null,
+  //     read: false
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "bibidi bobidi boo wah",
+  //     orig_module: "Management",
+  //     orig_submodule: "Access Control",
+  //     read: false
 
-    },
-    {
-      time: "8:00 PM",
-      msg: "Elit aliqua laborum laboris ex sint consectetur. Consequat dolor irure ullamco dolore adipisicing est labore velit. Amet cupidatat magna laboris commodo minim.",
-      orig_module: "Accounting",
-      orig_submodule: "Manufacturing Process",
-      read: false
-    },
-    {
-      time: "8:00 PM",
-      msg: "wowee!",
-      orig_module: "Accounting",
-      orig_submodule: "Accounts Receivable",
-      read: false
-    },
-    {
-      time: "8:00 PM",
-      msg: "Non incididunt commodo consequat occaecat proident consequat non.",
-      orig_module: "Accounting",
-      orig_submodule: "Accounts Receivable",
-      read: true
-    },
-    {
-      time: "8:00 PM",
-      msg: "Elit aliqua laborum laboris ex sint consectetur. Consequat dolor irure ullamco dolore adipisicing est labore velit. Amet cupidatat magna laboris commodo minim.",
-      orig_module: "Accounting",
-      orig_submodule: "Manufacturing Process",
-      read: true
-    },
-    {
-      time: "8:00 PM",
-      msg: "yippee!",
-      orig_module: "Accounting",
-      orig_submodule: "Accounts Receivable",
-      read: true
-    },
-    {
-      time: "8:00 PM",
-      msg: "Minim amet et non irure quis ea Lorem et dolor et tempor excepteur est.",
-      orig_module: "Accounting",
-      orig_submodule: "Accounts Receivable",
-      read: true
-    }
-  ];
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "Elit aliqua laborum laboris ex sint consectetur. Consequat dolor irure ullamco dolore adipisicing est labore velit. Amet cupidatat magna laboris commodo minim.",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Manufacturing Process",
+  //     read: false
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "wowee!",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Accounts Receivable",
+  //     read: false
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "Non incididunt commodo consequat occaecat proident consequat non.",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Accounts Receivable",
+  //     read: true
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "Elit aliqua laborum laboris ex sint consectetur. Consequat dolor irure ullamco dolore adipisicing est labore velit. Amet cupidatat magna laboris commodo minim.",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Manufacturing Process",
+  //     read: true
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "yippee!",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Accounts Receivable",
+  //     read: true
+  //   },
+  //   {
+  //     time: "8:00 PM",
+  //     msg: "Minim amet et non irure quis ea Lorem et dolor et tempor excepteur est.",
+  //     orig_module: "Accounting",
+  //     orig_submodule: "Accounts Receivable",
+  //     read: true
+  //   }
+  // ];
 
   // hooks for loading modules
   useEffect(() => {
@@ -578,10 +641,14 @@ function App() {
                   <div className={notif.read ? "notif-item" : "notif-item-unread"}
                     onClick={
                       notif.orig_submodule ? () => {
+                        notifs[i].read = true
+                        readNotif(notif.id)
                         setActiveModule(notif.orig_module)
                         setActiveSubModule(notif.orig_submodule)
                       }
                         : () => {
+                          notifs[i].read = true
+                          readNotif(notif.id)
                           setActiveModule(notif.orig_module)
                           setActiveSubModule(null)
                         }
