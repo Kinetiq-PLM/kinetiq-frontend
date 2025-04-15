88dcfb33d0f228abef7a2a2cd250cf607da57fe8
CoderTofu <vioneverforget@gmail.com>
2025-04-15 23:45:19 +0800

    fix: minor update

--- src/modules/CRM/CRM.jsx
+++ src/modules/CRM/CRM.jsx
@@ -12,9 +12,9 @@ const BodyContent = ({ loadSubModule, setActiveSubModule, employee_id }) => {
   };
 
   const type1 =
-    "h-[200px] min-w-[300px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg border p-10 shadow-md cursor-pointer font-bold text-2xl text-white bg-[#00A8A8] hover:scale-105 hover:shadow-md";
+    "h-[200px] min-w-[200px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg border p-10 shadow-md cursor-pointer font-bold text-2xl text-white bg-[#00A8A8] hover:scale-105 hover:shadow-md";
   const type2 =
-    "h-[200px] min-w-[300px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg p-10 shadow-md cursor-pointer font-bold text-2xl text-[#00A8A8] bg-[#E4FDFB] hover:scale-105 hover:shadow-lg";
+    "h-[200px] min-w-[200px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg p-10 shadow-md cursor-pointer font-bold text-2xl text-[#00A8A8] bg-[#E4FDFB] hover:scale-105 hover:shadow-lg";
 
   return (
     <div className="crm">
@@ -24,7 +24,7 @@ const BodyContent = ({ loadSubModule, setActiveSubModule, employee_id }) => {
           SubTitle="Your shortcut to all CRM operations."
         />
 
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 p-4 mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mb-4">
           {Object.entries(crmSubModule).map(([key, value], index) => {
             const buttonStyle = index % 2 === 0 ? type1 : type2;
             return (
--- src/modules/Sales/Sales.jsx
+++ src/modules/Sales/Sales.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+"use client";
+
+import { useEffect } from "react";
 import "./styles/Index.css";
 import Heading from "./components/Heading";
 
@@ -26,9 +28,9 @@ const BodyContent = ({ loadSubModule, setActiveSubModule, employee_id }) => {
   }, [action]);
 
   const type1 =
-    "h-[200px] min-w-[300px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg border p-10 shadow-md cursor-pointer font-bold text-2xl text-white bg-[#00A8A8] hover:scale-105 hover:shadow-md";
+    "h-[200px] min-w-[200px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg border p-10 shadow-md cursor-pointer font-bold text-2xl text-white bg-[#00A8A8] hover:scale-105 hover:shadow-md";
   const type2 =
-    "h-[200px] min-w-[300px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg p-10 shadow-md cursor-pointer font-bold text-2xl text-[#00A8A8] bg-[#E4FDFB] hover:scale-105 hover:shadow-lg";
+    "h-[200px] min-w-[200px] transition-all duration-300 ease-in-out transform flex items-center justify-center rounded-lg p-10 shadow-md cursor-pointer font-bold text-2xl text-[#00A8A8] bg-[#E4FDFB] hover:scale-105 hover:shadow-lg";
 
   return (
     <div className="sales">
@@ -43,7 +45,7 @@ const BodyContent = ({ loadSubModule, setActiveSubModule, employee_id }) => {
           SubTitle="Your shortcut to all sales operations."
         />
 
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4 p-4 mb-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4 mb-4">
           {Object.entries(salesSubModule).map(([key, value], index) => {
             const buttonStyle = index % 2 === 0 ? type1 : type2;
             return (
