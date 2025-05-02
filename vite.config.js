99b1a8814a047417b7d39cb89c32aa3009a9f870
Immaculate Fallaria <98595784+immakyulate@users.noreply.github.com>
2025-05-02 16:19:00 +0800

    fixed tailwind prob

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcssPlugin from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], 
+  plugins: [react()],
   base: './',
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPlugin,
+        autoprefixer,
+      ],
+    },
+  },
 })
