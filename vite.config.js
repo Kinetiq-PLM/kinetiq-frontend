5918600eafa46aba148675fda0a9902465b1c943
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2025-05-04 16:14:22 +0800

    fixed dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], 
-  base: './',
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPostcss(), // Use the new package and call it as a function
+        autoprefixer(),
+      ],
+    },
+  },
 })
