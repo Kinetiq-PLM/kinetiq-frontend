b416e3bc992350876ed1188c0db50b72fe7261d1
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2025-05-04 16:29:02 +0800

    tailwind ahh

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcssPostcss from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPostcss(), // Use the new package and call it as a function
+        autoprefixer(),
+      ],
+    },
+  },
 })
